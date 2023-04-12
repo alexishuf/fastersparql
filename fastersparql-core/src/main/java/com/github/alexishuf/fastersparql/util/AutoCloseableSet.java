11721f42d60e827072c2424776d9638f08d294cb
Alexis Armin Huf <alexishuf@gmail.com>
2023-04-11 21:52:02 -0300

    Add AutoCloseableSet.empty()

--- fastersparql-core/src/main/java/com/github/alexishuf/fastersparql/util/AutoCloseableSet.java
+++ fastersparql-core/src/main/java/com/github/alexishuf/fastersparql/util/AutoCloseableSet.java
@@ -6,8 +6,14 @@ import java.util.Collection;
 import static java.util.Arrays.asList;
 
 public class AutoCloseableSet<T extends AutoCloseable> extends ArrayList<T> implements AutoCloseable {
+    private static final AutoCloseableSet<?> EMPTY = new AutoCloseableSet<>();
+
     public AutoCloseableSet() { }
     public AutoCloseableSet(Collection<? extends T> c) { super(c); }
+
+    public static <T extends AutoCloseable> AutoCloseableSet<T> empty() { //noinspection unchecked
+        return (AutoCloseableSet<T>) EMPTY;
+    }
 
     @SafeVarargs
     public static <U extends AutoCloseable> AutoCloseableSet<U> of(U... instances) {
