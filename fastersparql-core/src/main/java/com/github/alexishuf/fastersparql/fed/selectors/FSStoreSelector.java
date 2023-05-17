93e30e7c6067a4b67282a023f6a2c7e1f69abcba
Alexis Armin Huf <alexishuf@gmail.com>
2023-05-17 12:21:04 -0300

    Add missing Loader to FSStoreSelector

--- fastersparql-core/src/main/java/com/github/alexishuf/fastersparql/fed/selectors/FSStoreSelector.java
+++ fastersparql-core/src/main/java/com/github/alexishuf/fastersparql/fed/selectors/FSStoreSelector.java
@@ -1,9 +1,14 @@
 package com.github.alexishuf.fastersparql.fed.selectors;
 
+import com.github.alexishuf.fastersparql.client.SparqlClient;
+import com.github.alexishuf.fastersparql.exceptions.BadSerializationException;
+import com.github.alexishuf.fastersparql.exceptions.BadSerializationException.SelectorTypeMismatch;
 import com.github.alexishuf.fastersparql.fed.Selector;
 import com.github.alexishuf.fastersparql.fed.Spec;
 import com.github.alexishuf.fastersparql.operators.plan.TriplePattern;
 import com.github.alexishuf.fastersparql.store.StoreSparqlClient;
+
+import java.io.IOException;
 
 public class FSStoreSelector extends Selector {
     public static final String NAME = "store";
@@ -12,6 +17,18 @@ public class FSStoreSelector extends Selector {
     public FSStoreSelector(StoreSparqlClient client, Spec spec) {
         super(client.endpoint(), spec);
         this.client = client.liveRef();
+        notifyInit(InitOrigin.LOAD, null);
+    }
+
+    public static class FSStoreLoader implements Loader {
+        @Override
+        public Selector load(SparqlClient client, Spec spec) throws IOException, BadSerializationException {
+            if (!(client instanceof StoreSparqlClient ssc))
+                throw new SelectorTypeMismatch("StoreSparqlClient", client.getClass().toString());
+            return new FSStoreSelector(ssc, spec);
+        }
+
+        @Override public String name() { return NAME; }
     }
 
     @Override public void close() {
@@ -21,7 +38,5 @@ public class FSStoreSelector extends Selector {
 
     @Override public void saveIfEnabled() { /* no-op since we access the store directly */ }
 
-    @Override public boolean has(TriplePattern tp) {
-        return client.get().estimate(tp) > 0;
-    }
+    @Override public boolean has(TriplePattern tp) { return client.get().estimate(tp) > 0; }
 }
