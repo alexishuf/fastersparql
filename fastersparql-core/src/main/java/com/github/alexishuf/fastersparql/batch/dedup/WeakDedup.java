92b95e2177fbb054369bea1336f2eb87e640431e
Alexis Armin Huf <alexishuf@gmail.com>
2023-06-28 10:18:53 -0300

    Avoid race on WeakDedup.isDuplicate

--- fastersparql-core/src/main/java/com/github/alexishuf/fastersparql/batch/dedup/WeakDedup.java
+++ fastersparql-core/src/main/java/com/github/alexishuf/fastersparql/batch/dedup/WeakDedup.java
@@ -7,11 +7,23 @@ import com.github.alexishuf.fastersparql.batch.type.RowBucket;
 import com.github.alexishuf.fastersparql.util.ThrowingConsumer;
 import com.github.alexishuf.fastersparql.util.concurrent.ArrayPool;
 
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
 import java.util.Arrays;
 
 import static java.lang.Integer.numberOfLeadingZeros;
 
 public final class WeakDedup<B extends Batch<B>> extends Dedup<B> {
+    static VarHandle LOCK;
+    static {
+        try {
+            LOCK = MethodHandles.lookup().findVarHandle(WeakDedup.class, "plainLock", int.class);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+
     /** Rows in the set. This works as a list of buckets of size 1. */
     private final RowBucket<B> rows;
     /** Value such that {@code hash & mask} yields the bucket index for a given hash value */
@@ -20,6 +32,8 @@ public final class WeakDedup<B extends Batch<B>> extends Dedup<B> {
     private int[] bitset;
     /** {@code hash & bitsetMask} yields a <strong>bit</strong> index in {@code bitset} */
     private final int bitsetMask;
+
+    @SuppressWarnings("unused") private int plainLock;
 
     public WeakDedup(BatchType<B> batchType, int capacity, int cols) {
         super(batchType, cols);
@@ -62,26 +76,22 @@ public final class WeakDedup<B extends Batch<B>> extends Dedup<B> {
      */
     @Override public boolean isDuplicate(B batch, int row, int source) {
         if (debug) checkBatchType(batch);
-        // beware of race conditions / lost updates
-        // the writes on bitset[] and row[] are not synchronized and one thread may undo
-        // updates done by another. For example, if thread 1 is doing add(r1) and thread 2 is
-        // doing add(r2) where r2 and r1 collide in bitset or rows, it may occur that after both
-        // exit add() returning true, only the side effects of thread 2 remain. A future call to
-        // add(r1) would then wrongly return true. However, this is allowed by the contract
-        // (wrong true return). What would not be allowed is a wrong false return which does
-        // not occur.
         int hash = batch.hash(row), bucket = hash&mask;
         int bitIdx = hash&bitsetMask, wordIdx = bitIdx >> 5, bit = 1 << bitIdx;
-        if ((bitset[wordIdx] & bit) != 0) { //row may be in set, we must compare
-            // in addition to bucket, also check bucket+1, since it may have received an evicted row
-            if (rows.equals(bucket, batch, row) || rows.equals(bucket+1, batch, row))
-                return true;
-        } else {
-            bitset[wordIdx] |= bit;
-        }
-        if (rows.has(bucket) && !rows.has(bucket+1))
-            rows.set(bucket+1, bucket); // if possible, delay eviction of old row at bucket
-        rows.set(bucket, batch, row);
+        while ((int)LOCK.compareAndExchangeAcquire(this, 0, 1) == 1)
+            Thread.yield();
+        try {
+            if ((bitset[wordIdx] & bit) != 0) { //row may be in set, we must compare
+                // bucket+1 may have received an evicted row
+                if (rows.equals(bucket, batch, row) || rows.equals(bucket + 1, batch, row))
+                    return true;
+            } else {
+                bitset[wordIdx] |= bit;
+            }
+            if (rows.has(bucket) && !rows.has(bucket + 1))
+                rows.set(bucket + 1, bucket); // if possible, delay eviction of old row at bucket
+            rows.set(bucket, batch, row);
+        } finally { LOCK.setRelease(this, 0); }
         return false;
     }
 
