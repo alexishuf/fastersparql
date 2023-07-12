f676e126ae9da5b6489cceb06ea505fde0b30fe7
Alexis Armin Huf <alexishuf@gmail.com>
2023-07-12 10:55:32 -0300

    Fix unstable test

--- fastersparql-core/src/test/java/com/github/alexishuf/fastersparql/batch/TimestampTest.java
+++ fastersparql-core/src/test/java/com/github/alexishuf/fastersparql/batch/TimestampTest.java
@@ -7,16 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TimestampTest {
 
-    @Test void testNoWait() {
-        for (int i = 0; i < 100_000; i++) {
-            long ts0 = Timestamp.nanoTime();
-            long ns0 = System.nanoTime();
-            long ts1 = Timestamp.nanoTime();
-            long ns1 = System.nanoTime();
-
-            assertTrue(ns0 > ts0, "Timestamp.nanoTime() ahead of System.nanoTime()");
-            assertTrue(ns1 > ts1, "Timestamp.nanoTime() ahead of System.nanoTime()");
-            assertTrue(ts1 >= ts0, "non-monotonic");
+    @Test void testMonotonic() {
+        for (int i = 0; i < 20; i++) {
+            Async.uninterruptibleSleep(50);
+            for (int j = 0; j < 1_000; j++) {
+                long ts0 = Timestamp.nanoTime();
+                long ts1 = Timestamp.nanoTime();
+                assertTrue(ts1 >= ts0, "non-monotonic");
+            }
         }
     }
 
