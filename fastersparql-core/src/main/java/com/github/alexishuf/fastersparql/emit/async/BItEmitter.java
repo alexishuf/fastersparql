5d97f1662c1d8bb6c702840444a4a8f6a372d578
Alexis Armin Huf <alexishuf@gmail.com>
2024-03-16 22:31:49 -0300

    Reimplement BItEmitter to rely on a virtual thread

--- fastersparql-core/src/main/java/com/github/alexishuf/fastersparql/emit/async/BItEmitter.java
+++ fastersparql-core/src/main/java/com/github/alexishuf/fastersparql/emit/async/BItEmitter.java
@@ -1,68 +1,200 @@
 package com.github.alexishuf.fastersparql.emit.async;
 
 import com.github.alexishuf.fastersparql.batch.BIt;
+import com.github.alexishuf.fastersparql.batch.BItReadCancelledException;
+import com.github.alexishuf.fastersparql.batch.base.SPSCBIt;
 import com.github.alexishuf.fastersparql.batch.type.Batch;
+import com.github.alexishuf.fastersparql.batch.type.BatchType;
+import com.github.alexishuf.fastersparql.emit.Emitter;
+import com.github.alexishuf.fastersparql.emit.EmitterStats;
+import com.github.alexishuf.fastersparql.emit.Emitters;
+import com.github.alexishuf.fastersparql.emit.Receiver;
+import com.github.alexishuf.fastersparql.emit.exceptions.MultipleRegistrationUnsupportedException;
 import com.github.alexishuf.fastersparql.emit.exceptions.RebindException;
+import com.github.alexishuf.fastersparql.emit.exceptions.RegisterAfterStartException;
 import com.github.alexishuf.fastersparql.model.Vars;
 import com.github.alexishuf.fastersparql.sparql.binding.BatchBinding;
 import com.github.alexishuf.fastersparql.util.StreamNode;
+import com.github.alexishuf.fastersparql.util.StreamNodeDOT;
+import com.github.alexishuf.fastersparql.util.concurrent.Async;
 import com.github.alexishuf.fastersparql.util.concurrent.ResultJournal;
+import com.github.alexishuf.fastersparql.util.concurrent.Unparker;
+import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Optional;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+import java.util.concurrent.locks.LockSupport;
 import java.util.stream.Stream;
 
-public final class BItEmitter<B extends Batch<B>> extends TaskEmitter<B> {
-    private final BIt<B> it;
+import static com.github.alexishuf.fastersparql.emit.Emitters.handleEmitError;
+import static com.github.alexishuf.fastersparql.util.concurrent.ThreadJournal.journal;
 
-    public BItEmitter(BIt<B> it) { this(EmitterService.EMITTER_SVC, RR_WORKER, it); }
-    public BItEmitter(EmitterService runner, int worker, BIt<B> it) {
-        super(it.batchType(), it.vars(), runner, worker, CREATED, TASK_FLAGS);
+public final class BItEmitter<B extends Batch<B>> extends Stateful
+        implements Emitter<B>, Runnable {
+    private static final Logger log = LoggerFactory.getLogger(BItEmitter.class);
+    private static final VarHandle REQ;
+    static {
+        try {
+            REQ = MethodHandles.lookup().findVarHandle(BItEmitter.class, "plainRequested", long.class);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    private final BIt<B> it;
+    private final Thread drainer;
+    private @MonotonicNonNull Receiver<B> downstream;
+    @SuppressWarnings("unused") private long plainRequested;
+    private final EmitterStats stats = EmitterStats.createIfEnabled();
+
+    public BItEmitter(BIt<B> it) {
+        super(CREATED, Flags.DEFAULT);
         this.it = it;
+        (this.drainer = Thread.ofVirtual().unstarted(this)).start();
         if (ResultJournal.ENABLED)
-            ResultJournal.initEmitter(this, vars);
+            ResultJournal.initEmitter(this, it.vars());
     }
 
     @Override protected void doRelease() {
-        super.doRelease();
-        it.close();
+        try {
+            it.close();
+        } finally {super.doRelease();}
     }
 
-    @Override public String toString() { return it.toString(); }
+    /* --- --- --- StreamNode --- --- --- */
 
-    @Override public Stream<? extends StreamNode> upstreamNodes() {
-        return Optional.ofNullable(it).stream();
+    @Override public String toString() { return label(StreamNodeDOT.Label.MINIMAL); }
+
+    @Override public Stream<? extends StreamNode> upstreamNodes() {return Stream.of(it);}
+
+    @Override public String label(StreamNodeDOT.Label type) {
+        String itLabel = it.label(StreamNodeDOT.Label.MINIMAL);
+        var sb = new StringBuilder(itLabel.length()+16).append("Em(");
+        sb.append(itLabel).append(')');
+        if (type.showState())
+            sb.append("st=").append(flags.render(state()));
+        if (EmitterStats.ENABLED && type.showStats() && stats != null)
+            sb = stats.appendToLabel(sb);
+        return sb.toString();
     }
 
+    /* --- --- --- --- Emitter --- --- --- */
+
+    @Override public Vars         vars()         {return it.vars();}
+    @Override public BatchType<B> batchType()    {return it.batchType();}
+    @Override public Vars         bindableVars() {return Vars.EMPTY;}
+    @Override public boolean        isComplete() {return Stateful.isCompleted(state());}
+    @Override public boolean       isCancelled() {return Stateful.isCancelled(state());}
+    @Override public boolean          isFailed() {return Stateful.isFailed(state());}
+    @Override public boolean      isTerminated() {return (state()&IS_TERM) != 0;}
+
+    @Override
+    public void subscribe(Receiver<B> receiver) throws RegisterAfterStartException, MultipleRegistrationUnsupportedException {
+        int st = lock(statePlain());
+        try {
+            if ((st&IS_INIT) == 0)
+                throw new RegisterAfterStartException(this);
+            if (EmitterStats.ENABLED && stats != null)
+                ++stats.receivers;
+            if (downstream != null && downstream != receiver)
+                throw new MultipleRegistrationUnsupportedException(this);
+            downstream = receiver;
+        } finally { unlock(st); }
+        journal("subscribed", receiver, "to", this);
+    }
+
+    @Override public boolean cancel() {
+        if (moveStateRelease(statePlain(), CANCEL_REQUESTED)) {
+            Unparker.unpark(drainer);
+            it.tryCancel();
+            return true;
+        }
+        return false;
+    }
+
+    @Override public void request(long rows) throws NoReceiverException {
+        int st = statePlain();
+        if ((st&IS_INIT) != 0 && moveStateRelease(st, ACTIVE))
+            st = st&FLAGS_MASK | ACTIVE;
+        if ((st&IS_LIVE) != 0 && Async.maxRelease(REQ, this, rows)) {
+            journal("request ", rows, ", unpark drainer on", this);
+            Unparker.unpark(drainer);
+        }
+    }
+
+    @Override public void rebindAcquire() {delayRelease();}
+    @Override public void rebindRelease() {allowRelease();}
     @Override public void rebind(BatchBinding binding) throws RebindException {
         throw new UnsupportedOperationException("Cannot rebind a BIt");
     }
 
-     @Override public Vars bindableVars() { return Vars.EMPTY; }
+    /* --- --- --- drainer --- --- --- */
 
-    @Override public boolean cancel() {
-        return it.tryCancel();
-    }
-
-    /* --- --- --- Task methods --- --- --- */
-
-    @Override protected int produceAndDeliver(int state) {
-        if (requested() <= 0)
-            return state;
-        int termState = state;
-        B b;
+    @Override public void run() {
+        if (Thread.currentThread() != drainer)
+            throw new IllegalStateException("Called from wrong thread");
+        SPSCBIt<B> queue = it instanceof SPSCBIt<B> q ? q : null;
+        int st = statePlain();
+        Throwable error = null;
+        long req = 0;
+        B b = null;
         try {
-            b = it.nextBatch(null);
-            if (b == null)
-                termState = COMPLETED;
+            while ((st&(IS_CANCEL_REQ|IS_TERM)) == 0) {
+                if ((req=(long)REQ.getAcquire(this)) <= 0) {
+                    LockSupport.park();
+                    st = stateAcquire();
+                } else {
+                    int iReq = (int) Math.min(Integer.MAX_VALUE, req);
+                    if (queue != null)
+                        queue.maxReadyItems(iReq);
+                    it.maxBatch(iReq);
+                    if ((b=it.nextBatch(b)) == null)
+                        break;
+                    REQ.getAndAddRelease(this, (long)-b.totalRows());
+                    try {
+                        if (ResultJournal.ENABLED)
+                            ResultJournal.logBatch(this, b);
+                        if (EmitterStats.ENABLED && stats != null)
+                            stats.onBatchDelivered(b);
+                        b = downstream.onBatch(b);
+                    } catch (Throwable t) {
+                        handleEmitError(downstream, this,
+                                        (statePlain()&IS_TERM) != 0, t);
+                    }
+                }
+            }
         } catch (Throwable t) {
-            b = null;
-            int st = state();
-            if      ((st&IS_CANCEL_REQ) != 0) termState = CANCELLED;
-            else if ((st&IS_TERM) != 0)       termState = st;
-            else                              throw t;
+            error = t;
+        } finally {
+            try {
+                it.close();
+            } catch (Throwable t) {
+                log.error("{} closing {} after end of iteration with error={}",
+                          t.getClass().getSimpleName(), it, error, t);
+            }
         }
-        if (b != null)
-            bt.recycleForThread(threadId, deliver(b));
-        return termState;
+        st = stateAcquire();
+        int termState;
+        if      (error instanceof BItReadCancelledException) termState = CANCELLED;
+        else if (error != null)                              termState = FAILED;
+        else if ((st&IS_CANCEL_REQ) != 0)                    termState = CANCELLED;
+        else                                                 termState = COMPLETED;
+        if (moveStateRelease(st, termState)) {
+            try {
+                switch (termState) {
+                    case COMPLETED -> downstream.onComplete();
+                    case FAILED    ->  downstream.onError(error);
+                    case CANCELLED -> downstream.onCancelled();
+                }
+            } catch (Throwable t) {
+                Emitters.handleTerminationError(downstream, this, t);
+            }
+            markDelivered(st, termState);
+        } else {
+            log.error("{} terminated outside drainer thread", this);
+            assert false : "another thread terminated BItEmitter state";
+        }
     }
 }
