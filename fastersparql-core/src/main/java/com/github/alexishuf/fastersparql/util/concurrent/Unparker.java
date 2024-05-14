d0cac671e4ba0682d0aa9f6c0f01544f6dffbb04
Alexis Armin Huf <alexishuf@gmail.com>
2024-05-14 04:09:12 -0300

    Faster Unparker by not unparking same thread twice

--- fastersparql-core/src/main/java/com/github/alexishuf/fastersparql/util/concurrent/Unparker.java
+++ fastersparql-core/src/main/java/com/github/alexishuf/fastersparql/util/concurrent/Unparker.java
@@ -1,6 +1,5 @@
 package com.github.alexishuf.fastersparql.util.concurrent;
 
-import org.jctools.queues.MessagePassingQueue;
 import org.jctools.queues.atomic.MpmcAtomicArrayQueue;
 
 import java.util.concurrent.locks.LockSupport;
@@ -17,15 +16,16 @@ public class Unparker {
         Timestamp.ON_TICK = new DrainOnTick();
     }
 
-    private static final Unpark UNPARK = new Unpark();
-    private static final class Unpark implements MessagePassingQueue.Consumer<Thread> {
-        @Override public void accept(Thread t) {LockSupport.unpark(t);}
-        @Override public String toString() {return "LockSupport.unpark()";}
-    }
     private static final class UnparkerTask implements Runnable {
         @SuppressWarnings("InfiniteLoopStatement") @Override public void run() {
             while (true) {
-                unparkQueue.drain(UNPARK);
+                Thread last = null, thread;
+                while ((thread=unparkQueue.poll()) != null) {
+                    if (thread != last) {
+                        LockSupport.unpark(thread);
+                        last = thread;
+                    }
+                }
                 LockSupport.park();
             }
         }
